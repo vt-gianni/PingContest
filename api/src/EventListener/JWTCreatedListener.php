8bc1b9fa540bdeb815e7c96ef1a0138285126d17
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2022-03-15 16:21:30 +0100

    :wheelchair: Ajout de données utilisateur dans le token renvoyé à la connexion

---
+++ api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Exception;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * @param JWTCreatedEvent $event
+     * @throws Exception
+     */
+    public function updateJwtData(JWTCreatedEvent $event)
+    {
+        /** @var User $user */
+        $user = $event->getUser();
+        $data = $event->getData();
+        $data['id'] = $user->getId();
+        $data['firstname'] = $user->getFirstname();
+        $data['lastname'] = $user->getLastname();
+        $data['birthdate'] = $user->getBirthdate();
+        $data['licenseNumber'] = $user->getLicenseNumber();
+        $data['officialPoints'] = $user->getOfficialPoints();
+
+        $event->setData($data);
+    }
+}
