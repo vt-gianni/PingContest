a80f90bca5343443d4dbbccf6beb99240589128d
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2022-05-17 12:32:28 +0200

    :coffin: Remove unused imports

--- api/tests/UserTest.php
+++ api/tests/UserTest.php
@@ -6,14 +6,13 @@ namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\User;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class UserTest extends ApiTestCase
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function testCreateUser(): void
     {
         $response = static::createClient()->request('POST', '/api/registration', ['json' => [
